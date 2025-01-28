6dc92093af6a0b28a6e29b885f1f10f50c116797
elimirze <47426829+elimirze@users.noreply.github.com>
2025-01-28 21:33:50 +0400

    Missing files for project

--- Forge/coalistic-forge-1.19.2-43.4.16/src/main/java/net/mstudio/coalistic/datagen/ModRecipeProvider.java
+++ Forge/coalistic-forge-1.19.2-43.4.16/src/main/java/net/mstudio/coalistic/datagen/ModRecipeProvider.java
@@ -1,31 +1,33 @@
 package net.mstudio.coalistic.datagen;
 
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.*;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import net.mstudio.coalistic.Coalistic;
-import net.mstudio.coalistic.block.CoalisticBlocks;
 import net.mstudio.coalistic.item.CoalisticItems;
 
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
-    public ModRecipeProvider(PackOutput pOutput) {
-        super(pOutput);
+    public ModRecipeProvider(DataGenerator generator) {
+        super(generator);
     }
     
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.COAL)
+    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(Items.COAL)
                 .pattern("***")
                 .pattern("* *")
                 .pattern("***")
                 .define('*', CoalisticItems.CoalPiece.get())
                 .unlockedBy(getHasName(CoalisticItems.CoalPiece.get()), has(CoalisticItems.CoalPiece.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_coal_piece_to_coal"));
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, CoalisticItems.DoubleCoal.get())
+        ShapedRecipeBuilder.shaped(CoalisticItems.DoubleCoal.get())
                 .pattern("***")
                 .pattern("*!*")
                 .pattern("***")
@@ -33,7 +35,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('!', Items.COAL)
                 .unlockedBy(getHasName(Items.COAL), has(Items.COAL))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "to_double_coal"));
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, CoalisticItems.TripleCoal.get())
+        ShapedRecipeBuilder.shaped(CoalisticItems.TripleCoal.get())
                 .pattern("***")
                 .pattern("*!*")
                 .pattern("***")
@@ -41,7 +43,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('!', CoalisticItems.DoubleCoal.get())
                 .unlockedBy(getHasName(CoalisticItems.DoubleCoal.get()), has(CoalisticItems.DoubleCoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "to_triple_coal"));
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, CoalisticItems.QuadrupleCoal.get())
+        ShapedRecipeBuilder.shaped(CoalisticItems.QuadrupleCoal.get())
                 .pattern("***")
                 .pattern("*!*")
                 .pattern("***")
@@ -49,14 +51,14 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('!', CoalisticItems.TripleCoal.get())
                 .unlockedBy(getHasName(CoalisticItems.TripleCoal.get()), has(CoalisticItems.TripleCoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "to_quadruple_coal"));
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.CHARCOAL)
+        ShapedRecipeBuilder.shaped(Items.CHARCOAL)
                 .pattern("* *")
                 .pattern("***")
                 .pattern("***")
                 .define('*', CoalisticItems.CoalPiece.get())
                 .unlockedBy(getHasName(CoalisticItems.CoalPiece.get()), has(CoalisticItems.CoalPiece.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_coal_piece_to_charcoal"));
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, CoalisticItems.DoubleCharcoal.get())
+        ShapedRecipeBuilder.shaped(CoalisticItems.DoubleCharcoal.get())
                 .pattern("***")
                 .pattern("*!*")
                 .pattern("***")
@@ -64,7 +66,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('!', Items.CHARCOAL)
                 .unlockedBy(getHasName(Items.CHARCOAL), has(Items.CHARCOAL))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "to_double_charcoal"));
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, CoalisticItems.TripleCharcoal.get())
+        ShapedRecipeBuilder.shaped(CoalisticItems.TripleCharcoal.get())
                 .pattern("***")
                 .pattern("*!*")
                 .pattern("***")
@@ -72,7 +74,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('!', CoalisticItems.DoubleCharcoal.get())
                 .unlockedBy(getHasName(CoalisticItems.DoubleCharcoal.get()), has(CoalisticItems.DoubleCharcoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "to_triple_charcoal"));
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, CoalisticItems.QuadrupleCharcoal.get())
+        ShapedRecipeBuilder.shaped(CoalisticItems.QuadrupleCharcoal.get())
                 .pattern("***")
                 .pattern("*!*")
                 .pattern("***")
@@ -80,35 +82,35 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('!', CoalisticItems.TripleCharcoal.get())
                 .unlockedBy(getHasName(CoalisticItems.TripleCharcoal.get()), has(CoalisticItems.TripleCharcoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "to_quadruple_charcoal"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),8)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),8)
                 .requires(Items.COAL)
                 .unlockedBy(getHasName(Items.COAL), has(Items.COAL))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_coal_to_coal_piece"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),16)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),16)
                 .requires(CoalisticItems.DoubleCoal.get())
                 .unlockedBy(getHasName(CoalisticItems.DoubleCoal.get()), has(CoalisticItems.DoubleCoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_double_coal_to_coal_piece"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),24)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),24)
                 .requires(CoalisticItems.TripleCoal.get())
                 .unlockedBy(getHasName(CoalisticItems.TripleCoal.get()), has(CoalisticItems.TripleCoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_triple_coal_to_coal_piece"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),32)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),32)
                 .requires(CoalisticItems.QuadrupleCoal.get())
                 .unlockedBy(getHasName(CoalisticItems.QuadrupleCoal.get()), has(CoalisticItems.QuadrupleCoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_quadruple_coal_to_coal_piece"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),8)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),8)
                 .requires(Items.CHARCOAL)
                 .unlockedBy(getHasName(Items.CHARCOAL), has(Items.CHARCOAL))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_charcoal_to_coal_piece"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),16)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),16)
                 .requires(CoalisticItems.DoubleCharcoal.get())
                 .unlockedBy(getHasName(CoalisticItems.DoubleCharcoal.get()), has(CoalisticItems.DoubleCharcoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_double_charcoal_to_coal_piece"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),24)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),24)
                 .requires(CoalisticItems.TripleCharcoal.get())
                 .unlockedBy(getHasName(CoalisticItems.TripleCharcoal.get()), has(CoalisticItems.TripleCharcoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_triple_charcoal_to_coal_piece"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CoalisticItems.CoalPiece.get(),32)
+        ShapelessRecipeBuilder.shapeless(CoalisticItems.CoalPiece.get(),32)
                 .requires(CoalisticItems.QuadrupleCharcoal.get())
                 .unlockedBy(getHasName(CoalisticItems.QuadrupleCharcoal.get()), has(CoalisticItems.QuadrupleCharcoal.get()))
                 .save(consumer,new ResourceLocation(Coalistic.MODID, "from_quadruple_charcoal_to_coal_piece"));
--- Forge/coalistic-forge-1.19.2-43.4.16/src/main/java/net/mstudio/coalistic/item/CoalisticItems.java
+++ Forge/coalistic-forge-1.19.2-43.4.16/src/main/java/net/mstudio/coalistic/item/CoalisticItems.java
@@ -7,16 +7,17 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.mstudio.coalistic.Coalistic;
 import net.mstudio.coalistic.item.custom.CoalisticFuels;
+import net.mstudio.coalistic.tabs.CoalisticCreativeTab;
 
 public class CoalisticItems {
     public static final DeferredRegister<Item> ITEMS =DeferredRegister.create(ForgeRegistries.ITEMS, Coalistic.MODID);
-    public static final RegistryObject<Item> CoalPiece= ITEMS.register("coal_piece",() -> new CoalisticFuels(new Item.Properties(), 200));
-    public static final RegistryObject<Item> DoubleCoal= ITEMS.register("double_coal",() -> new CoalisticFuels(new Item.Properties(), 3200));
-    public static final RegistryObject<Item> TripleCoal= ITEMS.register("triple_coal",() -> new CoalisticFuels(new Item.Properties(), 4800));
-    public static final RegistryObject<Item> QuadrupleCoal= ITEMS.register("quadruple_coal",() -> new CoalisticFuels(new Item.Properties(), 6400));
-    public static final RegistryObject<Item> DoubleCharcoal= ITEMS.register("double_charcoal",() -> new CoalisticFuels(new Item.Properties(), 3200));
-    public static final RegistryObject<Item> TripleCharcoal= ITEMS.register("triple_charcoal",() -> new CoalisticFuels(new Item.Properties(), 4800));
-    public static final RegistryObject<Item> QuadrupleCharcoal= ITEMS.register("quadruple_charcoal",() -> new CoalisticFuels(new Item.Properties(), 6400));
+    public static final RegistryObject<Item> CoalPiece= ITEMS.register("coal_piece",() -> new CoalisticFuels(new Item.Properties().tab(CoalisticCreativeTab.CoalisticTab), 200));
+    public static final RegistryObject<Item> DoubleCoal= ITEMS.register("double_coal",() -> new CoalisticFuels(new Item.Properties().tab(CoalisticCreativeTab.CoalisticTab), 3200));
+    public static final RegistryObject<Item> TripleCoal= ITEMS.register("triple_coal",() -> new CoalisticFuels(new Item.Properties().tab(CoalisticCreativeTab.CoalisticTab), 4800));
+    public static final RegistryObject<Item> QuadrupleCoal= ITEMS.register("quadruple_coal",() -> new CoalisticFuels(new Item.Properties().tab(CoalisticCreativeTab.CoalisticTab), 6400));
+    public static final RegistryObject<Item> DoubleCharcoal= ITEMS.register("double_charcoal",() -> new CoalisticFuels(new Item.Properties().tab(CoalisticCreativeTab.CoalisticTab), 3200));
+    public static final RegistryObject<Item> TripleCharcoal= ITEMS.register("triple_charcoal",() -> new CoalisticFuels(new Item.Properties().tab(CoalisticCreativeTab.CoalisticTab), 4800));
+    public static final RegistryObject<Item> QuadrupleCharcoal= ITEMS.register("quadruple_charcoal",() -> new CoalisticFuels(new Item.Properties().tab(CoalisticCreativeTab.CoalisticTab), 6400));
 
     public static void registerItems(IEventBus eventBus)
     {
