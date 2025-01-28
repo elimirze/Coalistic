7d1ee043375f0b771e12f682bfc88a5388c4d50d
elimirze <47426829+elimirze@users.noreply.github.com>
2025-01-28 21:33:50 +0400

    New Fabric Version

--- Forge/coalistic-forge-1.19.2-43.4.16/src/main/java/net/mstudio/coalistic/datagen/DataProviders.java
+++ Forge/coalistic-forge-1.19.2-43.4.16/src/main/java/net/mstudio/coalistic/datagen/DataProviders.java
@@ -1,40 +1,19 @@
 package net.mstudio.coalistic.datagen;
 
-import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.mstudio.coalistic.Coalistic;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
 @Mod.EventBusSubscriber(modid = Coalistic.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataProviders {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
-        PackOutput packOutput = generator.getPackOutput();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
-        generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
-                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK))));
-        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput));
-
-        BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
-        generator.addProvider(event.includeServer(), blockTagsProvider);
-        generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
-
-        generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
-        generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
-
+        generator.addProvider(event.includeServer(), new ModItemModelProvider(generator,existingFileHelper));
+        generator.addProvider(event.includeServer(),new ModRecipeProvider(generator));
     }
 }
